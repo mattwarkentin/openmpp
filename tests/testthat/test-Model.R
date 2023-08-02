1a0eb5b6ba01d8de1fcfeca3f96d04e66bbf2b85
Matt Warkentin <matthew.warkentin@ucalgary.ca>
2023-08-01 20:57:12 -0400

    Skip tests in some env

--- tests/testthat/test-Model.R
+++ tests/testthat/test-Model.R
@@ -1,4 +1,6 @@
 test_that("Retrieve models list", {
+  skip_on_ci()
+  skip_on_cran()
   expect_type(get_models_list(), 'list')
   expect_type(get_models(), 'list')
   expect_s3_class(get_models(), 'data.frame')
