2ba2207dcafc7333c05fb968f426ffebbf8d05e3
Matthew T. Warkentin <warkentin@lunenfeld.ca>
2025-01-07 19:08:14 -0700

    See if testing works on all OS

--- tests/testthat/helper-oms.R
+++ tests/testthat/helper-oms.R
@@ -1,8 +1,9 @@
 local_initiate_oms <- function(path) {
+  cmd <- if (Sys.info()['sysname'] == 'Windows') './bin/oms.exe' else './bin/oms'
   pid <-
     withr::with_dir(
       new = path,
-      code = sys::exec_background(cmd = './bin/oms', std_out = FALSE)
+      code = sys::exec_background(cmd = cmd, std_out = FALSE)
     )
   Sys.sleep(0.5)
   withr::defer_parent(tools::pskill(pid))
--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,10 +1,30 @@
 local_install_openmpp <- function() {
-  url <- 'https://github.com/openmpp/main/releases/latest/download/openmpp_mac_arm64_20241226.tar.gz'
+  info <- Sys.info()
+  os <- info['sysname']
+  type <- info['machine']
+
+  if (os == 'Darwin' & type == 'arm64') {
+    url <- 'https://github.com/openmpp/main/releases/latest/download/openmpp_mac_arm64_20241226.tar.gz'
+  } else if (os == 'Darwin' & type == 'intel') {
+    url <- 'https://github.com/openmpp/main/releases/latest/download/openmpp_mac_x86_64_20241226.tar.gz'
+  } else if (os == 'Linux') {
+    url <- 'https://github.com/openmpp/main/releases/latest/download/openmpp_debian_20241226.tar.gz'
+  } else if (os == 'Windows') {
+    url <- 'https://github.com/openmpp/main/releases/latest/download/openmpp_win_20241226.zip'
+  } else {
+    rlang::abort('No compatible OS detected.')
+  }
   file <- basename(url)
   dir <- tempdir()
   path <- paste0(dir, '/', file)
   download.file(url, path, quiet = TRUE)
-  untar(path, exdir = dir)
+
+  if (os == 'Windows') {
+    unzip(path, exdir = dir)
+  } else {
+    untar(path, exdir = dir)
+  }
+
   unlink(path)
   new_path <- tools::file_path_sans_ext(path, compression = TRUE)
   new_path
